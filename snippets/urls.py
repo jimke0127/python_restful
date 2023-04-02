02548e72101f085215c2eb5ebe84b60a05aed5f1
jack <jimke127@126.com>
2023-04-02 18:30:28 +0800

    123

---
+++ snippets/urls.py
@@ -0,0 +1,13 @@
+from django.urls import re_path, path
+from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken import views as v
+
+urlpatterns = [
+    re_path(r'^snippets/$', views.SnippetList.as_view()),
+    re_path(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+    re_path(r'^users/$', views.UserList.as_view()),
+    re_path(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    re_path(r'^api-token-auth/', v.obtain_auth_token)  # 生成用户token，用post，需要账号和密码，所以在登录的时候可以返回给前端
+]
+urlpatterns = format_suffix_patterns(urlpatterns)
---
+++ snippets/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render
+
+# Create your views here.
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from snippets.serializers import UserSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+class SnippetList(APIView):
+
+    # permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    """
+    列出所有的snippets或者创建一个新的snippet。
+    """
+    def get(self, request, format=None):
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class SnippetDetail(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    """
+    检索，更新或删除一个snippet示例。
+    """
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = SnippetSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
